2fbfde949cdfda55bf1c3d63984b32c170ae3547
Lisgevan <episimo@hotmail.com>
2023-12-04 20:20:53 +0200

    08_Working_with_Refs_and_Portals - section finished

--- 08_Working_with_Refs_and_Portals/01-starting-project/src/components/Player.jsx
+++ 08_Working_with_Refs_and_Portals/01-starting-project/src/components/Player.jsx
@@ -1,11 +1,43 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const playerName = useRef();
+
+	const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+	// const [submitted, setSubmitted] = useState(false);
+
+	// const handleChange = event => {
+	// 	setSubmitted(false);
+	// 	setEnteredPlayerName(event.target.value);
+	// };
+	// const handleClick = () => setSubmitted( true );
+
+	// all the comments can be changed to the following using refs
+	const handleClick = () => {
+		if (playerName.current.value === "") setEnteredPlayerName(null);
+		else setEnteredPlayerName(playerName.current.value);
+		playerName.current.value = "";
+	};
+
+	return (
+		<section id="player">
+			{/* <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2> */}
+			<h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+			{/* (enteredPlayerName ?? "unknown entity")
+          is the same as 
+          (enteredPlayerName ? enteredPlayerName "unknown entity") */}
+			<p>
+				<input
+					ref={playerName}
+					type="text"
+					//a;so the following are not needed any more
+					// onChange={ handleChange }
+					// value={ enteredPlayerName }
+				/>
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
+
