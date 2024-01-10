301f6b6dcaf95840fd4997782cc47108dbc9f4c7
Lisgevan <episimo@hotmail.com>
2024-01-10 21:30:23 +0200

    20_Advanced_Redux -  section finished

--- 20_Advanced_Redux/src/App_using_async_in_components.js
+++ 20_Advanced_Redux/src/App_using_async_in_components.js
@@ -1,7 +1,7 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { sendCartData, fetchCartData } from "./store/cart-actions";
+import { uiActions } from "./store/ui-slice";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -17,18 +17,49 @@ function App() {
 	const notification = useSelector(state => state.ui.notification);
 
 	useEffect(() => {
-		dispatch(fetchCartData());
-	}, [dispatch]);
+		const sendCartData = async () => {
+			dispatch(
+				uiActions.showNotification({
+					status: "pending",
+					title: "Sending...",
+					message: "Sending cart data!",
+				})
+			);
+			const response = await fetch(
+				"https://rtcg-advancedreduxproject-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+				{
+					method: "PUT",
+					body: JSON.stringify(cart),
+				}
+			);
 
-	useEffect(() => {
+			if (!response.ok) {
+				throw new Error("Sending cart data failed");
+			}
+
+			dispatch(
+				uiActions.showNotification({
+					status: "success",
+					title: "Success!",
+					message: "Sent cart data successfully!",
+				})
+			);
+		};
+
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
 
-		if (cart.changed) {
-			dispatch(sendCartData(cart));
-		}
+		sendCartData().catch(error => {
+			dispatch(
+				uiActions.showNotification({
+					status: "error",
+					title: "Error!",
+					message: "Sending cart data failed!",
+				})
+			);
+		});
 	}, [cart, dispatch]);
 
 	return (
--- 20_Advanced_Redux/src/components/Shop/Products.js
+++ 20_Advanced_Redux/src/components/Shop/Products.js
@@ -1,19 +1,39 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
-const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+const DUMMY_PRODUCTS = [
+	{
+		id: 1,
+		price: 6,
+		title: "My first Book",
+		description: "My first book I ever wrote.",
+	},
+	{
+		id: 2,
+		price: 5,
+		title: "My second Book",
+		description: "My second book I ever wrote.",
+	},
+];
+
+const Products = props => {
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_PRODUCTS.map(product => (
+					<ProductItem
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						price={product.price}
+						escription={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
+
