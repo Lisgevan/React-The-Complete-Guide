1ebdbf977dab0edbbdaefbe8b1d21cc81e883173
Lisgevan <episimo@hotmail.com>
2024-01-12 19:07:12 +0200

    21_React_Router - Basic Routing part finished

--- 21_React_Router/Basic_Routing_Project/src/App.js
+++ 21_React_Router/Basic_Routing_Project/src/App.js
@@ -1,5 +1,48 @@
+// //second way to define the routes
+// import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+
+// import HomePage from "./pages/Home";
+// import ProductsPage from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+// 	<Route>
+// 		<Route path="/" element={<HomePage />} />
+// 		<Route path="/products" element={<ProductsPage />} />
+// 	</Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+// function App() {
+// 	return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <HomePage /> }, // same as writing path:"/"
+			{ path: "products", element: <ProductsPage /> },
+			{ path: "products/:productId", element: <ProductDetailPage /> },
+		],
+	},
+]);
+
 function App() {
-  return <div></div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
+
