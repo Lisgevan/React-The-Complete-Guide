238c56862ab829d22a12669d143b6a6944164e07
Lisgevan <episimo@hotmail.com>
2024-01-17 20:19:14 +0200

    22_Authentication_To_React - section complete

--- 22_Authentication_To_React/frontend/src/pages/Root.js
+++ 22_Authentication_To_React/frontend/src/pages/Root.js
@@ -1,19 +1,40 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+import { useEffect } from "react";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+	const token = useLoaderData();
+	const submit = useSubmit();
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+		if (token === "EXPIRED") {
+			submit(null, { action: "/logout", method: "POST" });
+			return;
+		}
+
+		const tokenDuretion = getTokenDuration();
+		console.log(tokenDuretion);
+
+		setTimeout(() => {
+			submit(null, { action: "/logout", method: "POST" });
+		}, tokenDuretion);
+	}, [token, submit]);
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default RootLayout;
+
